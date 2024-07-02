d011e2871e28e2fa471010fb5a5346f6beb95f10
ZulfikarHD <zulfikar.h9050@gmail.com>
2024-07-02 15:36:52 +0700

    template view

---
+++ resources/js/Pages/Owner/OwBookingManagement.vue
@@ -0,0 +1,8 @@
+<script setup>
+import AuthenticatedLayout from "@/Layouts/Owner/OwAuthenticatedLayout.vue";
+import { Head } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <AuthenticatedLayout> </AuthenticatedLayout>
+</template>
