97ada001d4aa1ce593d7d9b5946a2b75ab9ed9dd
ZulfikarHD <zulfikar.h9050@gmail.com>
2024-07-17 16:28:28 +0700

    up

---
+++ resources/js/Components/DekstopNavigation.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Navigation" />
+
+    <div class="container mx-auto z-50">
+        <div class="flex justify-between items-center py-6">
+            <!-- Logo -->
+            <div class="logo">
+                <img :src="route('asset', 'img/logo.png')" alt="logo">
+            </div>
+
+            <!-- Link -->
+            <div class="flex justify-center gap-6 items-center">
+                <a href="#">Home</a>
+                <a href="#">Game</a>
+                <a href="#">Lapangan</a>
+            </div>
+
+            <!-- Button -->
+            <div class="flex justify-center gap-6 items-center">
+                <a href="#">Masuk</a>
+                <a class="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition ease-in-out duration-200 active:ring-2 active:ring-green-600" href="#">Daftar</a>
+            </div>
+        </div>
+    </div>
+</template>
