215a7ffdc69635efe885ea8a3465aa6ba1b6edce
ZulfikarHD <zulfikar.h9050@gmail.com>
2024-07-22 23:31:54 +0700

    add cari lapangan controller,page and route/link from navigation

---
+++ resources/js/Components/Card/ItemCard.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-white rounded-lg shadow-lg py-4 px-6 flex justify-between items-center space-x-4 w-full max-w-6xl drop-shadow-sm">
+        <slot />
+    </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CariLapanganController;
 use App\Http\Controllers\Owner\FinancialManagement\OwFmBudgetingController;
 use App\Http\Controllers\Owner\FinancialManagement\OwFmExpensesController;
 use App\Http\Controllers\Owner\FinancialManagement\OwFmInvoicesController;
@@ -34,6 +35,7 @@ Route::get('/owner', function () {
 })->name('owner.dashboard');
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/cari-lapangan', [CariLapanganController::class, 'index'])->name('home');
 
 // Route::get('/dashboard', function () {
 //     return Inertia::render('Dashboard');
