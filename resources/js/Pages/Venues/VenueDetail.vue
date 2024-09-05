17ab0424e177f11c9a27d578d4b6b8ef7b26b3ab
ZulfikarHD <zulfikar.h9050@gmail.com>
2024-09-05 22:08:33 +0700

    add card lapangan

--- resources/js/Pages/Venues/VenueDetail.vue
+++ resources/js/Pages/Venues/VenueDetail.vue
@@ -294,9 +294,135 @@ const scrollLeft = () => {
         </section>
 
         <!-- List Venue Section -->
-         <section
-         class="max-w-6xl mx-auto mt-4 mb-6 px-4 lg:px-0 pb-6 border-b border-slate-300">
+        <section
+            class="max-w-6xl mx-auto mt-4 mb-6 px-4 lg:px-0 pb-6 border-b border-slate-200"
+        >
+            <div
+                class="flex justify-between gap-6 flex-wrap bg-white flex-1 rounded-lg shadow-md drop-shadow-lg overflow-hidden w-full relative hover:bg-slate-100 transition ease-in-out duration-200 p-4"
+            >
+                <!-- Image Venue -->
+                <div class="flex flex-col gap-4 w-full md:w-fit">
+                    <img
+                        src=""
+                        alt="field Pic"
+                        class="w-full min-w-0 md:min-w-96 h-48 bg-slate-400 rounded-lg shadow drop-shadow"
+                    />
 
-         </section>
+                    <div class="lg:grid grid-cols-2 hidden gap-4">
+                        <img
+                            src=""
+                            alt="field Pic"
+                            class="w-48 h-24 bg-slate-400 rounded-lg shadow drop-shadow"
+                        />
+                        <img
+                            src=""
+                            alt="field Pic"
+                            class="w-48 h-24 bg-slate-400 rounded-lg shadow drop-shadow"
+                        />
+                    </div>
+                </div>
+
+                <!-- Field Available-->
+                <div class="flex flex-col w-full flex-1 px-4">
+                    <!-- Venue Description -->
+                    <div
+                        class="pt-4 pb-6 border-b border-slate-200 flex justify-between flex-wrap gap-4"
+                    >
+                        <div>
+                            <!-- Venue Name -->
+                            <h1 class="font-bold text-xl">Lapangan 1</h1>
+
+                            <!-- Venue Description -->
+                            <div class="mt-4 max-w-[30ch]">
+                                <p
+                                    class="text-sm font-light text-slate-500 line-clamp-3"
+                                >
+                                    Lorem ipsum dolor sit amet consectetur,
+                                    adipisicing elit. Accusamus possimus nisi
+                                    vero perspiciatis error eligendi quaerat
+                                    eius quam est minus!
+                                </p>
+                            </div>
+                        </div>
+
+                        <!-- Sports Available -->
+                        <div class="flex flex-wrap gap-2 items-baseline mt-1.5">
+                            <!-- {{-- Futsal --}} -->
+                            <a
+                                href="#"
+                                class="relative border border-green-600/70 px-2 py-0.5 rounded-md overflow-clip flex gap-2"
+                            >
+                                <div
+                                    class="bg-green-500 rounded-xl -left-1 inset-y-0 h-[20px] w-[28px] z-0 ml-0.5 absolute"
+                                ></div>
+                                <img
+                                    src="/svg/soccer-ball.svg"
+                                    class="absolute w-4 h-4 left-0 ml-1 fill-green-50"
+                                />
+                                <span class="text-xs pl-6 text-green-800"
+                                    >Futsal</span
+                                >
+                            </a>
+
+                            <!-- {{-- Badminton --}} -->
+                            <a
+                                href="#"
+                                class="relative border border-green-600/70 px-2 py-0.5 rounded-md overflow-clip flex gap-2"
+                            >
+                                <div
+                                    class="bg-green-500 rounded-xl -left-1 inset-y-0 h-[20px] w-[28px] z-0 ml-0.5 absolute"
+                                ></div>
+                                <img
+                                    src="/svg/badminton.svg"
+                                    class="absolute w-4 h-4 left-0 ml-1"
+                                />
+                                <span class="text-xs pl-6 text-green-800"
+                                    >Badminton</span
+                                >
+                            </a>
+                        </div>
+                    </div>
+
+                    <!-- fasilitas -->
+                    <div
+                        class="pt-4 pb-6 border-b border-slate-200 flex justify-between flex-wrap gap-4"
+                    >
+                        <h1 class="font-bold text-xl">Fasilitas</h1>
+                        <div class="flex gap-4 flex-wrap">
+                            <!-- {{-- Fasilitas Badge --}} -->
+                            <div
+                                class="flex gap-4 items-center flex-wrap"
+                            >
+                                <!-- {{-- Rumput --}} -->
+                                <div
+                                    class="flex gap-2 bg-slate-500 px-2 py-1 rounded-md"
+                                >
+                                    <!-- {{-- Svg Icon --}} -->
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="w-4 h-4 fill-slate-50 text-slate-50"
+                                        viewBox="0 0 60 60"
+                                    >
+                                        <path
+                                            fill="#f8fafc"
+                                            fill-rule="evenodd"
+                                            d="M55 34a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h2a1 1 0 1 0 0-2h-1v-8h1Zm0-8H45a1 1 0 0 0-1 1v22a1 1 0 0 0 1 1h10a1 1 0 1 0 0-2h-9V28h9a1 1 0 1 0 0-2ZM5 42a1 1 0 1 0 0 2h2a1 1 0 0 0 1-1V33a1 1 0 0 0-1-1H5a1 1 0 1 0 0 2h1v8H5Zm0 8h10a1 1 0 0 0 1-1V27a1 1 0 0 0-1-1H5a1 1 0 1 0 0 2h9v20H5a1 1 0 1 0 0 2ZM20.853 7.976l3.276 1.064L29 5.491v-3.44c-3.65.364-6.724 2.701-8.147 5.925ZM31 2.051V5.49l4.884 3.545 3.263-1.059C37.724 4.752 34.65 2.415 31 2.051ZM40 12c0-.729-.083-1.438-.232-2.123l-3.269 1.061-1.88 5.894 2.011 2.634C38.691 17.633 40 14.969 40 12ZM30 22c1.826 0 3.535-.5 5.01-1.359L32.993 18h-6.015l-1.993 2.638A9.929 9.929 0 0 0 30 22Zm-4.532-11.46L27.201 16h5.584l1.746-5.475-4.53-3.289-4.533 3.304Zm-2.101 8.923 1.996-2.642-1.867-5.883-3.264-1.061A9.978 9.978 0 0 0 20 12c0 2.968 1.307 5.63 3.367 7.463ZM18 12c0-1.287.209-2.524.585-3.687l.003-.021c.003-.01.011-.017.015-.026C20.177 3.474 24.688 0 30 0c5.314 0 9.827 3.476 11.399 8.271.003.008.009.013.012.021.002.005 0 .011.002.017C41.791 9.473 42 10.712 42 12c0 3.949-1.926 7.448-4.879 9.636-.018.017-.028.041-.049.056-.027.021-.059.03-.089.047a11.915 11.915 0 0 1-13.96.005c-.03-.018-.063-.026-.091-.048-.022-.016-.033-.042-.053-.059C19.926 19.448 18 15.949 18 12Zm11 29.858v-7.716c-1.72.447-3 2-3 3.858s1.28 3.411 3 3.858Zm2-7.716v7.716c1.721-.447 3-2 3-3.858s-1.279-3.411-3-3.858ZM29 55V43.91c-2.833-.478-5-2.943-5-5.91s2.167-5.432 5-5.91V27a1 1 0 1 1 2 0v5.09c2.833.478 5 2.943 5 5.91s-2.167 5.432-5 5.91V55a1 1 0 1 1-2 0Zm31-34v34c0 2.757-2.243 5-5 5H5c-2.757 0-5-2.243-5-5V21c0-2.757 2.243-5 5-5h11a1 1 0 1 1 0 2H5c-1.654 0-3 1.346-3 3v34c0 1.654 1.346 3 3 3h50c1.654 0 3-1.346 3-3V21c0-1.654-1.346-3-3-3H44a1 1 0 1 1 0-2h11c2.757 0 5 2.243 5 5Z"
+                                        />
+                                    </svg>
+
+                                    <!-- {{-- Text --}} -->
+                                    <span
+                                        class="text-xs text-slate-50 font-light tracking-wide"
+                                        >Rumput Sintetis</span
+                                    >
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </section>
     </AppLayout>
 </template>
