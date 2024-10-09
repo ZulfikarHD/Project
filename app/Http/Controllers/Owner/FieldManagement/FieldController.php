de046febc686dfb8bb1a8ac6ebbbc44327507192
ZulfikarHD <zulfikar.h9050@gmail.com>
2024-10-09 16:31:36 +0700

    bck

--- app/Http/Controllers/Owner/FieldManagement/FieldController.php
+++ app/Http/Controllers/Owner/FieldManagement/FieldController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Owner\FieldManagement;
 
 use App\Http\Controllers\Controller;
+use App\Models\VenueField;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -15,6 +16,10 @@ class FieldController extends Controller
 
     public function fieldData()
     {
-
+        // venue,field_name,lokasi,gambar,sports_available
+        $venue = VenueField::query()
+                        ->join('venues','venue_fields.venue_id','venues.venue_id')
+                        ->join('field_images','venue_fields.field_id','field_images.field_id')
+                        ->join('venue_field_sports','venue_fields.field_id','venue_field_sports.venue_field_id');
     }
 }
